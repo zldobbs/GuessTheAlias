638471c6db4742bb4376212e31bd39cf61327273
Trevor Leach <tleachman1234@gmail.com>
2017-10-14 15:09:06 -0500

    refactor multiple room routes into one

--- GTA/main.py
+++ GTA/main.py
@@ -22,22 +22,32 @@ def LandingPage():
 def AboutPage():
 	return render_template('about.html')
 
+"""
 @app.route('/createRoom', methods=['GET']) # change to createRoom at frontend
 def createRoom():
 	roomCode = ''.join(choice(ascii_uppercase + digits) for i in range(6))
-	return redirect(url_for('.RoomPage', code=roomCode, moderator=True))
+	creator = session['userID']
+	return redirect(url_for('.RoomPage', code=roomCode, creator=creator))
 
 @app.route('/joinRoom', methods=['POST']) # change to createRoom at frontend
 def joinRoom():
 	roomCode = request.form["roomCode"]
 	# check for existing room before redirecting
-	return redirect(url_for('.RoomPage', code=roomCode, moderator=False))
+	return redirect(url_for('.RoomPage', code=roomCode))
+"""
 
-@app.route('/room', methods=['GET'])
+@app.route('/room', methods=['GET', 'POST'])
 def RoomPage():
-	roomCode = request.args['code']
-	return render_template('waitingRoom.html', code=roomCode)
-	# return roomCode + ' ' + session['userID']
+	if request.form['action'] == 'create':
+		roomCode = ''.join(choice(ascii_uppercase + digits) for i in range(6))
+		creator = session['userID']
+		return render_template('waitingRoomMod.html', code=roomCode)
+
+	if request.form['action'] == 'join':
+		roomCode = request.form["roomCode"]
+		return render_template('waitingRoomUser.html', code=roomCode)
+
+	return 'Error: Incorrect action'
 
 @socketio.on('connect', namespace='/room')
 def connected():
@@ -46,7 +56,6 @@ def connected():
 @socketio.on('getUserID', namespace='/getUserID')
 def GetUserID():
 	emit('UserID response', {'userID': session['userID']})
-
 
 
 
