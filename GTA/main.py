e534dba9512871b22e5882b889434e37288a4352
Trevor Leach <tleachman1234@gmail.com>
2017-10-13 23:06:15 -0500

    fix merge conflict

--- GTA/main.py
+++ GTA/main.py
@@ -1,46 +1,26 @@
 #!flask/bin/python
 import os, json
-<<<<<<< HEAD
 from flask import Flask, render_template
 from flask import request
 from random import choice
 from string import ascii_uppercase, digits
-=======
-from flask import Flask
-from flask import request
->>>>>>> Views
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def LandingPage():
-<<<<<<< HEAD
 	return render_template('LandingPage.html')
-=======
-	pass
->>>>>>> Views
 
 @app.route('/about', methods=['GET'])
 def AboutPage():
 	pass
 
-<<<<<<< HEAD
 @app.route('/generateCode', methods=['GET'])
 def GenCode():
 	return ''.join(choice(ascii_uppercase + digits) for i in range(6))
 
-
 @app.route('/<code>', methods=['GET'])
 def RoomPage(code):
-=======
-@app.route('/generateCode', methods=['POST'])
-def GenCode():
-	# return roomcode
-	pass
-
-@app.route('/<code>', methods=['GET'])
-def RoomPage():
->>>>>>> Views
 	pass
 
 if __name__ == "__main__":
