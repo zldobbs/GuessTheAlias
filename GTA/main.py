45df6e92e31431201244e7cf680fddff882b6e80
Trevor Leach <tleachman1234@gmail.com>
2017-10-14 20:50:04 -0500

    add database functionality

--- GTA/main.py
+++ GTA/main.py
@@ -11,6 +11,55 @@ app = Flask(__name__)
 app.secret_key = 'FF137EE9744FFBEFC1495D3FA08A639FD9F7B57330212F30B470EA2BFB5CCF5B'
 socketio = SocketIO(app)
 
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy_gta.db'
+db = SQLAlchemy(app)
+
+
+class Rooms(db.Model):
+	roomID = db.Column(db.String(6), primary_key=True)
+	roomState = db.Column(db.String(8), nullable=True)
+	currentAlias_FK = db.Column(db.String(16), nullable=True)
+	roomCreator_FK = db.Column(db.String(16), nullable=False)
+
+	def __repr__(self):
+		return '<roomID: %r | Mod: %r>' % (self.roomID, self.roomCreator_FK)
+
+class Teams(db.Model):
+	color = db.Column(db.String(13), primary_key=True) 
+	rooms_FK = db.Column(db.String(6), primary_key=True)
+	Score = db.Column(db.Integer)
+
+	def __repr__(self):
+		return '<room: %r | color: %r>' % (self.roomID, self.color)
+
+class Players(db.Model):
+	userID = db.Column(db.String(16), primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	hint1 = db.Column(db.Text, nullable=True)
+	hint2 = db.Column(db.Text, nullable=True)
+	hint3 = db.Column(db.Text, nullable=True)
+	hint4 = db.Column(db.Text, nullable=True)
+	hint5 = db.Column(db.Text, nullable=True)
+	hint6 = db.Column(db.Text, nullable=True)
+	readyStatus = db.Column(db.Boolean, nullable=False)
+	color_FK = db.Column(db.String(13), nullable=True)
+	rooms_FK = db.Column(db.String(6), nullable=False)
+
+	def __repr__(self):
+		return '<userID: %r | Name: %r>' % (self.userID, self.name)
+
+db.create_all()
+
+
+
+
+
 @app.route('/', methods=['GET'])
 def LandingPage():
 	if 'userID' not in session:
@@ -42,7 +91,30 @@ def joinRoom(data):
 def createRoom(data):
 	roomCode = ''.join(choice(ascii_uppercase + digits) for i in range(6))
 	join_room(roomCode)
-	# save session['userID'] into DB
+
+	playerEntry = Players(
+		userID = session['userID'],
+		name = str(data['name']),
+		hint1 = '',
+		hint2 = '',
+		hint3 = '',
+		hint4 = '',
+		hint5 = '',
+		hint6 = '',
+		readyStatus = False,
+		color_FK = 'red',
+		rooms_FK = roomCode
+	)
+	roomEntry = Rooms(
+		roomID = roomCode,
+		roomState = 'lobby',
+		currentAlias_FK = '',
+		roomCreator_FK = session['userID']
+	)
+	db.session.add(roomEntry)
+	db.session.add(playerEntry)
+	db.session.commit()
+
 	emit('roomResponse', {'roomCode': roomCode})
 
 @app.route('/room/<roomCode>', methods=['GET'])
@@ -57,8 +129,7 @@ def roomPage(roomCode):
 def startGame(data):
 	roomCode = data['roomCode']
 	if userIsMod(roomCode, session['userID']):
-		pass
-	### emit('roomResponse', {'roomCode': roomCode})
+		emit('beginGame', room=roomCode)
 
 def roomExists(roomCode):
 	return True
